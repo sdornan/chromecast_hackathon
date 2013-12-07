48410c30f8cf958d759567ad93584d16a53421b4
Sam Dornan <sdornan@gmail.com>
2013-12-07 12:59:37 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChromecastHackathon::Application.config.secret_key_base = '20a356c84673e63e041b0d6613a19b8e03c930c2ead4f97c7b646bda48f0601dbd513c4cf3e4dfd5759117b2b12837f5673855eb9d9467e8830011f7d6303a58'
