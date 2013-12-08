0f048c70e12c6548d626480963d9fb0e9a048311
James Bobowski <james@fandor.com>
2013-12-07 16:09:08 -0800

    Changed to Presentation, added new custom commands

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -28,28 +28,12 @@ var cast = window.cast || {};
 (function() {
   'use strict';
 
-  TicTacToe.PROTOCOL = 'YOUR_PROTOCOL_HERE';
+  Presentation.PROTOCOL = 'urn:x-cast:com.fandor.demo.tictactoe';
 
-  TicTacToe.PLAYER = {
-    O: 'O',
-    X: 'X'
-  };
-
-  /**
-   * Creates a TicTacToe object.
-   * @param {board} board an optional game board.
-   * @constructor
-   */
-  function TicTacToe(board) {
-    this.mBoard = board;
-    this.mPlayer1 = -1;
-    this.mPlayer2 = -1;
-    this.mCurrentPlayer;
-
-    console.log('********TicTacToe********');
+  function Presentation() {
     this.castReceiverManager_ = cast.receiver.CastReceiverManager.getInstance();
     this.castMessageBus_ =
-        this.castReceiverManager_.getCastMessageBus(TicTacToe.PROTOCOL,
+        this.castReceiverManager_.getCastMessageBus(Presentation.PROTOCOL,
         cast.receiver.CastMessageBus.MessageType.JSON);
     this.castMessageBus_.onMessage = this.onMessage.bind(this);
     this.castReceiverManager_.onSenderConnected =
@@ -59,9 +43,7 @@ var cast = window.cast || {};
     this.castReceiverManager_.start();
   }
 
-
-  // Adds event listening functions to TicTacToe.prototype.
-  TicTacToe.prototype = {
+  Presentation.prototype = {
 
     /**
      * Sender Connected event
@@ -95,224 +77,34 @@ var cast = window.cast || {};
       var message = event.data;
       var senderId = event.senderId;
       console.log('********onMessage********' + JSON.stringify(event.data));
-      console.log('mPlayer1: ' + this.mPlayer1);
-      console.log('mPlayer2: ' + this.mPlayer2);
 
-      if (message.command == 'join') {
-        this.onJoin(senderId, message);
-      } else if (message.command == 'leave') {
-        this.onLeave(senderId);
-      } else if (message.command == 'move') {
-        this.onMove(senderId, message);
-      } else if (message.command == 'board_layout_request') {
-        this.onBoardLayoutRequest(senderId);
+      if (message.command == 'load') {
+        this.onLoad();
+      } else if (message.command == 'prev') {
+        this.onPrev();
+      } else if (message.command == 'next') {
+        this.onNext();
       } else {
         console.log('Invalid message command: ' + message.command);
       }
     },
 
-    /**
-     * Player joined event: registers a new player who joined the game, or
-     * prevents player from joining if invalid.
-     * @param {string} senderId the sender the message came from.
-     * @param {Object|string} message the name of the player who just joined.
-     */
-    onJoin: function(senderId, message) {
-      console.log('****onJoin****');
-
-      if ((this.mPlayer1 != -1) &&
-          (this.mPlayer1.senderId == senderId)) {
-        this.sendError(senderId, 'You are already ' +
-                       this.mPlayer1.player +
-                       ' You aren\'t allowed to play against yourself.');
-        return;
-      }
-      if ((this.mPlayer2 != -1) &&
-          (this.mPlayer2.senderId == senderId)) {
-        this.sendError(senderId, 'You are already ' +
-                       this.mPlayer2.player +
-                       ' You aren\'t allowed to play against yourself.');
-        return;
-      }
-
-      if (this.mPlayer1 == -1) {
-        this.mPlayer1 = new Object();
-        this.mPlayer1.name = message.name;
-        this.mPlayer1.senderId = senderId;
-      } else if (this.mPlayer2 == -1) {
-        this.mPlayer2 = new Object();
-        this.mPlayer2.name = message.name;
-        this.mPlayer2.senderId = senderId;
-      } else {
-        console.log('Unable to join a full game.');
-        this.sendError(senderId, 'Game is full.');
-        return;
-      }
-
-      console.log('mPlayer1: ' + this.mPlayer1);
-      console.log('mPlayer2: ' + this.mPlayer2);
-
-      if (this.mPlayer1 != -1 && this.mPlayer2 != -1) {
-        this.mBoard.reset();
-        this.startGame_();
-      }
+    onLoad: function() {
+      $('#debug').append("<div>onLoad</div>");
     },
 
-    /**
-     * Player leave event: determines which player left and unregisters that
-     * player, and ends the game if all players are absent.
-     * @param {string} senderId the sender ID of the leaving player.
-     */
-    onLeave: function(senderId) {
-      console.log('****OnLeave****');
-
-      if (this.mPlayer1 != -1 && this.mPlayer1.senderId == senderId) {
-        this.mPlayer1 = -1;
-      } else if (this.mPlayer2 != -1 && this.mPlayer2.senderId == senderId) {
-        this.mPlayer2 = -1;
-      } else {
-        console.log('Neither player left the game');
-        return;
-      }
-      console.log('mBoard.GameResult: ' + this.mBoard.getGameResult());
-      if (this.mBoard.getGameResult() == -1) {
-        this.mBoard.setGameAbandoned();
-        this.broadcastEndGame(this.mBoard.getGameResult());
-      }
+    onPrev: function() {
+      $('#debug').append("<div>onPrev</div>");
     },
 
-    /**
-     * Move event: checks whether a valid move was made and updates the board
-     * as necessary.
-     * @param {string} senderId the sender that made the move.
-     * @param {Object|string} message contains the row and column of the move.
-     */
-    onMove: function(senderId, message) {
-      console.log('****onMove****');
-      var isMoveValid;
-
-      if ((this.mPlayer1 == -1) || (this.mPlayer2 == -1)) {
-        console.log('Looks like one of the players is not there');
-        console.log('mPlayer1: ' + this.mPlayer1);
-        console.log('mPlayer2: ' + this.mPlayer2);
-        return;
-      }
-
-      if (this.mPlayer1.senderId == senderId) {
-        if (this.mPlayer1.player == this.mCurrentPlayer) {
-          if (this.mPlayer1.player == TicTacToe.PLAYER.X) {
-            isMoveValid = this.mBoard.drawCross(message.row, message.column);
-          } else {
-            isMoveValid = this.mBoard.drawNaught(message.row, message.column);
-          }
-        } else {
-          console.log('Ignoring the move. It\'s not your turn.');
-          this.sendError(senderId, 'It\'s not your turn.');
-          return;
-        }
-      } else if (this.mPlayer2.senderId == senderId) {
-        if (this.mPlayer2.player == this.mCurrentPlayer) {
-          if (this.mPlayer2.player == TicTacToe.PLAYER.X) {
-            isMoveValid = this.mBoard.drawCross(message.row, message.column);
-          } else {
-            isMoveValid = this.mBoard.drawNaught(message.row, message.column);
-          }
-        } else {
-          console.log('Ignoring the move. It\'s not your turn.');
-          this.sendError(senderId, 'It\'s not your turn.');
-          return;
-        }
-      } else {
-        console.log('Ignorning message. Someone other than the current' +
-            'players sent a move.');
-        this.sendError(senderId, 'You are not playing the game');
-        return;
-      }
-
-      if (isMoveValid === false) {
-        this.sendError(senderId, 'Your last move was invalid');
-        return;
-      }
-
-      var isGameOver = this.mBoard.isGameOver();
-      this.broadcast({
-        event: 'moved',
-        player: this.mCurrentPlayer,
-        row: message.row,
-        column: message.column,
-        game_over: isGameOver });
-
-      console.log('isGameOver: ' + isGameOver);
-      console.log('winningLoc: ' + this.mBoard.getWinningLocation());
-
-      // When the game should end
-      if (isGameOver == true) {
-        this.broadcastEndGame(this.mBoard.getGameResult(),
-            this.mBoard.getWinningLocation());
-      }
-      // Switch current player
-      this.mCurrentPlayer = (this.mCurrentPlayer == TicTacToe.PLAYER.X) ?
-          TicTacToe.PLAYER.O : TicTacToe.PLAYER.X;
-    },
-
-    /**
-     * Request event for the board layout: sends the current layout of pieces
-     * on the board through the channel.
-     * @param {string} senderId the sender the event came from.
-     */
-    onBoardLayoutRequest: function(senderId) {
-      console.log('****onBoardLayoutRequest****');
-      var boardLayout = [];
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          boardLayout[i * 3 + j] = this.mBoard.mBoard[i][j];
-        }
-      }
-      this.castMessageBus_.send(senderId, {
-        'event': 'board_layout_response',
-        'board': boardLayout });
+    onNext: function() {
+      $('#debug').append("<div>onNext</div>");
     },
 
     sendError: function(senderId, errorMessage) {
       this.castMessageBus_.send(senderId, {
         'event': 'error',
         'message': errorMessage });
-    },
-
-    broadcastEndGame: function(endState, winningLocation) {
-      console.log('****endGame****');
-      this.mPlayer1 = -1;
-      this.mPlayer2 = -1;
-      this.broadcast({
-        event: 'endgame',
-        end_state: endState,
-        winning_location: winningLocation });
-    },
-
-    /**
-     * @private
-     */
-    startGame_: function() {
-      console.log('****startGame****');
-      var firstPlayer = Math.floor((Math.random() * 10) % 2);
-      this.mPlayer1.player = (firstPlayer === 0) ?
-          TicTacToe.PLAYER.X : TicTacToe.PLAYER.O;
-      this.mPlayer2.player = (firstPlayer === 0) ?
-          TicTacToe.PLAYER.O : TicTacToe.PLAYER.X;
-      this.mCurrentPlayer = TicTacToe.PLAYER.X;
-
-      this.castMessageBus_.send(
-          this.mPlayer1.senderId, {
-            event: 'joined',
-            player: this.mPlayer1.player,
-            opponent: this.mPlayer2.name
-          });
-      this.castMessageBus_.send(
-          this.mPlayer2.senderId, {
-            event: 'joined',
-            player: this.mPlayer2.player,
-            opponent: this.mPlayer1.name
-          });
     },
 
     /**
@@ -326,5 +118,5 @@ var cast = window.cast || {};
   };
 
   // Exposes public functions and APIs
-  cast.TicTacToe = TicTacToe;
+  cast.Presentation = Presentation;
 })();
